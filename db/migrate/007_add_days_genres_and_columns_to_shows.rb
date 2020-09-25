b0f5386a5b05362dcb44116d3c294f01614b9767
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-09-25 04:26:43 +0000

    Automatically backed up by Learn

--- db/migrate/007_add_days_genres_and_columns_to_shows.rb
+++ db/migrate/007_add_days_genres_and_columns_to_shows.rb
@@ -1,5 +1,7 @@
 class AddDaysGenresAndColumnsToShows < ActiveRecord::Migration[5.1]
   def change
     add_column :shows, :days, :string
+    add_column :shows, :genres, :string
+    add_column :shows, :seasons, :string
   end
 end
