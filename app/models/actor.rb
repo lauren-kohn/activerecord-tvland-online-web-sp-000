aecafee3b8c4d1d1274fee1632e7bab745e2862d
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-09-25 04:09:50 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -7,7 +7,7 @@ class Actor < ActiveRecord::Base
   end 
   
   def list_roles 
-    [self.characters.name, self.characters.show].join(" - ")
+    [self.characters.name, self.characters.shows].join(" - ")
   end 
   
 end
